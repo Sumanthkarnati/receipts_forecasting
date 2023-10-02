db80889b1234dfc63b9cdd33b7d5befd0c5f83de
sumanth <karnati2@wisc.edu>
2023-10-01 20:10:04 -0700

    Adding working stream lit app

---
+++ algorithms.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.preprocessing import MinMaxScaler
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import matplotlib.pyplot as plt
+import joblib
+
+# Load and preprocess data
+
+
+def load_and_preprocess_data(filepath):
+    data = pd.read_csv(filepath)
+    data['# Date'] = pd.to_datetime(data['# Date'])
+    data.set_index('# Date', inplace=True)
+    return data  # return daily data
+
+
+# LSTM Model
+def lstm_model(filepath):
+    data = load_and_preprocess_data(filepath)
+    train_size = int(len(data) * 0.8)
+    train, test = data[:train_size], data[train_size:]
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_train = scaler.fit_transform(train)
+    scaled_test = scaler.transform(test)
+    joblib.dump(scaler, 'lstm_scaler.pkl')
+
+    X_train, y_train = [], []
+    for i in range(len(scaled_train) - 1):
+        X_train.append(scaled_train[i:(i + 1), 0])
+        y_train.append(scaled_train[i + 1, 0])
+    X_train, y_train = np.array(X_train), np.array(y_train)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(
+        X_train.shape[1], X_train.shape[2]), return_sequences=True))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(50))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(X_train, y_train, epochs=50, batch_size=1, verbose=0)
+    model.save("lstm_model.h5")
+
+    test_predictions = []
+    first_eval_batch = scaled_train[-1:]
+    current_batch = first_eval_batch.reshape((1, 1, 1))
+    
+    for i in range(len(test)):
+        current_pred = model.predict(current_batch)[0]
+        test_predictions.append(current_pred)
+        current_batch = np.append(current_batch[:, 1:, :], [[current_pred]], axis=1)
+    
+    true_predictions = scaler.inverse_transform(np.array(test_predictions).reshape(-1, 1))
+    
+    mae = mean_absolute_error(test, true_predictions)
+    rmse = np.sqrt(mean_squared_error(test, true_predictions))
+
+    # Save the scaler, MAE, and RMSE
+    joblib.dump(mae, 'lstm_mae.pkl')
+    joblib.dump(rmse, 'lstm_rmse.pkl')
+    return mae, rmse, model, scaler
+
+
+# Gradient Boosting Model
+def gradient_boosting_model(filepath):
+    data = load_and_preprocess_data(filepath)
+    train_size = int(len(data) * 0.8)
+    train, test = data[:train_size], data[train_size:]
+
+    X_train, y_train = pd.DataFrame(index=train.index), train['Receipt_Count']
+    X_test = pd.DataFrame(index=test.index)
+
+    for i in range(1, 4):
+        X_train[f'lag_{i}'] = train['Receipt_Count'].shift(i)
+    X_train.dropna(inplace=True)
+    y_train = y_train.loc[X_train.index]
+
+    for i in range(1, 4):
+        X_test[f'lag_{i}'] = test['Receipt_Count'].shift(i)
+
+    # Handle cases where X_test might end up being empty after dropping NaN values
+    if X_test.dropna().empty:
+        raise ValueError(
+            "Not enough data points in the test set to create lag features.")
+
+    X_test.dropna(inplace=True)
+
+    model = GradientBoostingRegressor(
+        n_estimators=100, learning_rate=0.1, random_state=0)
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+    mae = mean_absolute_error(test.loc[X_test.index], predictions)
+    rmse = np.sqrt(mean_squared_error(test.loc[X_test.index], predictions))
+    return mae, rmse, model
+
+
+# Exponential Smoothing State Space Model (ETS)
+def ets_model(filepath):
+    data = load_and_preprocess_data(filepath)
+    train_size = int(len(data) * 0.8)
+    train, test = data[:train_size], data[train_size:]
+
+    model = ExponentialSmoothing(np.asarray(
+        train['Receipt_Count']), seasonal='add', seasonal_periods=12)
+    fit = model.fit()
+
+    predictions = fit.forecast(len(test))
+
+    mae = mean_absolute_error(test, predictions)
+    rmse = np.sqrt(mean_squared_error(test, predictions))
+    return mae, rmse, fit
+
+
+def predict_lstm(model, scaler, n_days):
+    data = load_and_preprocess_data('data_daily.csv')
+    predictions = []
+    first_eval_batch = scaler.transform(data[-1:])
+    current_batch = first_eval_batch.reshape((1, 1, 1))
+
+    for i in range(n_days):
+        current_pred = model.predict(current_batch)[0]
+        predictions.append(current_pred)
+        current_batch = np.append(current_batch[:, 1:, :], [
+            [current_pred]], axis=1)
+    return scaler.inverse_transform(np.array(predictions).reshape(-1, 1)).flatten()
+
+
+def predict_gb(model, n_days):
+    data = load_and_preprocess_data('data_daily.csv')
+    predictions = []
+
+    for i in range(n_days):
+        last_known_values = data['Receipt_Count'].tail(3).values.reshape(1, -1)
+        prediction = model.predict(last_known_values)[0]
+        predictions.append(prediction)
+        # Adjusted to days for daily data
+        new_index = data.index[-1] + pd.DateOffset(days=1)
+        data = data.append(pd.DataFrame(
+            {'Receipt_Count': [prediction]}, index=[new_index]))
+
+    return predictions
+
+
+def predict_ets(model, n_days):
+    return model.forecast(steps=n_days)
--- app.py
+++ app.py
@@ -1,92 +1,95 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from statsmodels.tsa.stattools import acf
+import seaborn as sns
+import base64
+# Replace with actual module and function names
+from algorithms import lstm_model, gradient_boosting_model, ets_model, predict_lstm, predict_gb, predict_ets
+from tensorflow.keras.models import load_model
+import joblib
+import os
+
+# Load Data
 
 
-data = pd.read_csv('data_daily.csv')
-data['Date'] = pd.to_datetime(data['Date'])
-data.set_index('Date', inplace=True)
+@st.cache
+def load_data(filepath):
+    data = pd.read_csv(filepath)
+    data['# Date'] = pd.to_datetime(data['# Date'])
+    return data
 
-
-def plot_time_series():
-    plt.figure(figsize=(12, 6))
-    plt.plot(data.index, data['Receipt_Count'], label='Receipt Count')
-    plt.title('Daily Scanned Receipts in 2021')
-    plt.xlabel('Date')
-    plt.ylabel('Receipt Count')
-    plt.grid(True)
-    plt.legend()
-    plt.tight_layout()
-    st.pyplot()
-
-
-def plot_acf():
-    lag_acf = acf(data['Receipt_Count'], nlags=40, fft=False)
-    plt.figure(figsize=(12, 6))
-    plt.plot(lag_acf, marker='o')
-    plt.axhline(y=0, linestyle='--', color='gray')
-    plt.axhline(
-        y=-1.96/np.sqrt(len(data['Receipt_Count'])), linestyle='--', color='gray')
-    plt.axhline(
-        y=1.96/np.sqrt(len(data['Receipt_Count'])), linestyle='--', color='gray')
-    plt.title('Autocorrelation Function (ACF)')
-    plt.xlabel('Lag')
-    plt.ylabel('Autocorrelation')
-    plt.tight_layout()
-    st.pyplot()
-
-
-def plot_forecasted_2022():
-    plt.figure(figsize=(12, 6))
-    plt.bar(forecasted_2022.keys(), forecasted_2022.values(), color='teal')
-    plt.title('Forecasted Scanned Receipts in 2022')
-    plt.xlabel('Month')
-    plt.ylabel('Receipt Count')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    st.pyplot()
+# Main Function to run the app
 
 
 def main():
-    st.title("Scanned Receipts Forecasting App")
+    st.title("Receipts Forecasting App")
 
     st.sidebar.header("Navigation")
     page = st.sidebar.radio(
-        "Go to", ["Home", "Time Series Analysis", "Forecasting", "New Data Prediction"])
+        "Go to", ["Home", "Data Exploration", "Model Predictions", "Future Predictions"])
 
     if page == "Home":
-        st.header("Welcome!")
-        st.write("This app allows you to view the time series analysis, forecasting results, and make new predictions for the number of scanned receipts.")
+        st.header("Welcome to Receipts Forecasting App!")
+        st.write("Navigate to different pages using the sidebar.")
 
-    elif page == "Time Series Analysis":
-        st.header("Time Series Analysis of Daily Scanned Receipts in 2021")
+    if page == "Data Exploration":
+        st.header("Data Exploration")
+
+        # Load and display data
+        data = load_data('data_daily.csv')
+        st.write(data.head())
+
+        # Display plots
+        st.subheader("Time Series Plot")
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.lineplot(data=data, x='# Date', y='Receipt_Count', ax=ax)
+        st.pyplot(fig)
+
+    if page == "Model Predictions":
+        st.header("Model Predictions")
+        # Run models and display results
+
+        try:
+            model_lstm = load_model('lstm_model.h5')
+            scaler_lstm = joblib.load('lstm_scaler.pkl')
+            mae_lstm = joblib.load('lstm_mae.pkl')
+            rmse_lstm = joblib.load('lstm_rmse.pkl')
+        except:
+            mae_lstm, rmse_lstm, model_lstm, scaler_lstm = lstm_model(
+                "data_daily.csv")
+
+        st.write(f"LSTM Model - MAE: {mae_lstm}, RMSE: {rmse_lstm}")
+
+        mae_gb, rmse_gb, model_gb = gradient_boosting_model(
+            'data_daily.csv')
+        st.write(f"Gradient Boosting Model - MAE: {mae_gb}, RMSE: {rmse_gb}")
+
+        mae_ets, rmse_ets, model_ets = ets_model('data_daily.csv')
+        st.write(f"ETS Model - MAE: {mae_ets}, RMSE: {rmse_ets}")
+
+        if page == "Future Predictions":
+            st.header("Future Predictions")
+
+        dates_2022 = pd.date_range(
+            start='2022-01-01', end='2022-12-31', freq='D')
+        future_df = pd.DataFrame(dates_2022, columns=['Date'])
+
+        # Assuming 'scaler' is the MinMaxScaler used during training
+        future_df['LSTM_Predictions'] = predict_lstm(
+            model_lstm, scaler_lstm, 365)
+        future_df['GB_Predictions'] = predict_gb(model_gb, 365)
+        future_df['ETS_Predictions'] = predict_ets(model_ets, 365)
+
         st.write(
-            "This section provides a visual analysis of the daily scanned receipts data for the year 2021.")
-        plot_time_series()
-        st.write(
-            "Autocorrelation Function (ACF) is used to identify the seasonality in the time series data.")
-        plot_acf()
+            "Here you can view the models' predictions for future receipts in 2022.")
+        st.write(future_df)
 
-    elif page == "Forecasting":
-        st.header("Forecasting Scanned Receipts for 2022")
-        st.write("This section shows the forecasted monthly total of scanned receipts for the year 2022 using an Exponential Smoothing model.")
-        plot_forecasted_2022()
-
-        month = st.selectbox(
-            "Select a Month to View Forecasted Receipts", list(forecasted_2022.keys()))
-        st.write(
-            f"Forecasted Receipt Count for {month} 2022: {forecasted_2022[month]}")
-
-        st.write("Optimal Parameters:")
-        st.write(f"Alpha (Level Smoothing Parameter): 0.19")
-        st.write(f"Beta (Trend Smoothing Parameter): 0.13")
-
-    elif page == "New Data Prediction":
-        st.header("Predict New Data")
-        st.write("You can enter new data here and get the forecasted receipts.")
+        csv = future_df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()
+        href = f'<a href="data:file/csv;base64,{b64}" download="predictions_2022.csv">Download Predictions CSV File</a>'
+        st.markdown(href, unsafe_allow_html=True)
 
 
+# Run the app
 if __name__ == "__main__":
     main()
